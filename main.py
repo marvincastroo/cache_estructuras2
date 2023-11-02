98727a56fba8a84bfe2becff70436eefdf1e2f49
Anndress07 <92402988+Anndress07@users.noreply.github.com>
2023-11-02 15:26:59 -0600

    avances en processTrace

--- main.py
+++ main.py
@@ -45,14 +45,49 @@ def buildCache(data):
     linea, index, ways, cache_size = data
     # podemos hacer una sola matriz, que sea de tamaño filas = index, y columna = linea*ways
     cache = np.zeros((index, linea*ways))
+
     return cache
 
+# funcion que calcula la cantidad de bits necesarios para la direccion dado el tamaño de la cache
+def tagBlockBits(cache):
+    linea, index, ways, cache_size = cache
+    block_offset_bits = np.log2(linea)
+    index_bits = np.log2(index)
+    tag_bits = 32 - block_offset_bits - index_bits
+    print(block_offset_bits, index_bits, tag_bits)
+    return block_offset_bits, index_bits, tag_bits
 
 
-def processTrace():
+
+def processTrace(cache, data, address_bits):
+    # creación de máscaras para obtener tag, index y block offset dada la dirección
+
+    block_offset_bits, index_bits, tag_bits = int(address_bits[0]), int(address_bits[1]), int(address_bits[2])
+    mask_block_offset = (1 << block_offset_bits) - 1
+    mask_index_bits   = ((1 << index_bits) - 1) << block_offset_bits
+    mask_tag_bits     = ((1 << tag_bits) - 1) << (block_offset_bits + index_bits)
+
+    i = 0
     with open('trace.out', 'r') as file:
         for line in file:
+            i = i + 1
+            #print(i)
+            if (i < 5):
                 print(line)
+                line_splitted = line.split()
+                instruction_type = int(line_splitted[1])  # tipo de instrucción: 0 = load, 1 = store
+                address_value = int(line_splitted[2], 16) # dirección
+                address_hex = hex(address_value)
+                print(address_hex)
+
+                block_number = (address_value & mask_block_offset)
+                index_number = (address_value & mask_index_bits) >> block_offset_bits
+                tag_number = (address_value & mask_tag_bits) >> (block_offset_bits + index_bits)
+
+                print("block: ", hex(block_number))
+                print("index: ", hex(index_number))
+                print("tag: ", hex(tag_number))
+
 
     return
 
@@ -61,4 +96,6 @@ if __name__ == '__main__':
     #especificaciones_cache = getValues()
     data = 32, 256, 4, 32768
     cache = buildCache(data)
-    print(data)
+    address_bits = tagBlockBits(data)
+    processTrace(cache, data, address_bits)
+    #print(data)
